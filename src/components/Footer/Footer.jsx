9bb35c71b5d005a33921d4e4e201649b0eb8e4df
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-03-20 15:54:23 +0600

    added file setup

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <p>
+                <small>
+                    Thank you visiting our website
+                </small>
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
